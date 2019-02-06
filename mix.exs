62d2337c06152cdb867b5647a2a399ee661ab63d
Michael Bengtson <michael.bengtson@gmail.com>
2019-02-06 16:11:19 -0600

    Udpate
    Downgraded to spec Elixir 1.7 since 1.8 is not running on Pi.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule InternetQOS.Mixfile do
     [
       app: :internet_qos,
       version: "1.0.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
